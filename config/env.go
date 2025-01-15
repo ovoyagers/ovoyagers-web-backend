97a77a0844048063f95dd806e41e866369178cea
azar-writes-code <azharuddinmohammed998@gmail.com>
2025-01-15 23:57:16 +0530

    refactor: config file in prod to load environment vars
    Signed-off-by: azar-writes-code <azharuddinmohammed998@gmail.com>

--- config/env.go
+++ config/env.go
@@ -32,81 +32,83 @@ type Config struct {
 }
 
 // LoadConfig loads the configuration from the specified path.
-func LoadConfig() (c *Config, err error) {
-	environment, ok := os.LookupEnv("ENVIRONMENT")
-	if !ok {
-		environment = "local"
-	}
+func LoadConfig() (*Config, error) {
+    c := &Config{} // Properly initialize the Config struct
 
-	if environment == "local" {
-		viper.SetConfigFile(".env")
-		viper.AutomaticEnv() // Load matching environment variables automatically
+    environment, ok := os.LookupEnv("ENVIRONMENT")
+    if !ok {
+        environment = "local"
+    }
 
-		if err = viper.ReadInConfig(); err != nil {
-			log.Error("Error reading configuration file: ", err)
-			return nil, err
-		}
+    if environment == "local" {
+        viper.SetConfigFile(".env")
+        viper.AutomaticEnv() // Load matching environment variables automatically
 
-		if err = viper.Unmarshal(&c); err != nil {
-			log.Error("Error unmarshaling configuration: ", err)
-			return nil, err
-		}
-	} else {
-		// List of required environment variables
-		envVars := []string{
-			"ACCESS_TOKEN_SECRET", "REFRESH_TOKEN_SECRET", "PORT", "NEO4J_USER", "NEO4J_PASSWORD",
-			"NEO4J_URI", "TWILLIO_ACCOUNT_SID", "TWILLIO_ACCOUNT_PASSWORD", "TWILLIO_ACCOUNT_SERVICE_SID",
-			"EMAIL_HOST", "EMAIL_PORT", "EMAIL_USER", "EMAIL_PASSWORD", "IMAGEKIT_PUBLIC_KEY", "IMAGEKIT_PRIVATE_KEY", "IMAGEKIT_URL_ENDPOINT", "UPSTASH_REDIS_REST_URL", "UPSTASH_REDIS_REST_TOKEN",
-		}
+        if err := viper.ReadInConfig(); err != nil {
+            log.Error("Error reading configuration file: ", err)
+            return nil, err
+        }
 
-		// Load environment variables dynamically
-		for _, key := range envVars {
-			value := os.Getenv(key)
-			if value == "" {
-				log.Warnf("%s is not set", key)
-			}
+        if err := viper.Unmarshal(c); err != nil {
+            log.Error("Error unmarshaling configuration: ", err)
+            return nil, err
+        }
+    } else {
+        // List of required environment variables
+        envVars := []string{
+            "ACCESS_TOKEN_SECRET", "REFRESH_TOKEN_SECRET", "PORT", "NEO4J_USER", "NEO4J_PASSWORD",
+            "NEO4J_URI", "TWILLIO_ACCOUNT_SID", "TWILLIO_ACCOUNT_PASSWORD", "TWILLIO_ACCOUNT_SERVICE_SID",
+            "EMAIL_HOST", "EMAIL_PORT", "EMAIL_USER", "EMAIL_PASSWORD", "IMAGEKIT_PUBLIC_KEY", "IMAGEKIT_PRIVATE_KEY", "IMAGEKIT_URL_ENDPOINT", "UPSTASH_REDIS_REST_URL", "UPSTASH_REDIS_REST_TOKEN",
+        }
 
-			switch key {
-			case "ACCESS_TOKEN_SECRET":
-				c.ACCESS_TOKEN_SECRET = value
-			case "REFRESH_TOKEN_SECRET":
-				c.REFRESH_TOKEN_SECRET = value
-			case "PORT":
-				c.PORT = value
-			case "NEO4J_USER":
-				c.NEO4J_USER = value
-			case "NEO4J_PASSWORD":
-				c.NEO4J_PASSWORD = value
-			case "NEO4J_URI":
-				c.NEO4J_URI = value
-			case "TWILLIO_ACCOUNT_SID":
-				c.TWILLIO_ACCOUNT_SID = value
-			case "TWILLIO_ACCOUNT_PASSWORD":
-				c.TWILLIO_ACCOUNT_PASSWORD = value
-			case "TWILLIO_ACCOUNT_SERVICE_SID":
-				c.TWILLIO_ACCOUNT_SERVICE_SID = value
-			case "EMAIL_HOST":
-				c.EMAIL_HOST = value
-			case "EMAIL_PORT":
-				c.EMAIL_PORT = value
-			case "EMAIL_USER":
-				c.EMAIL_USER = value
-			case "EMAIL_PASSWORD":
-				c.EMAIL_PASSWORD = value
-			case "IMAGEKIT_PUBLIC_KEY":
-				c.IMAGEKIT_PUBLIC_KEY = value
-			case "IMAGEKIT_PRIVATE_KEY":
-				c.IMAGEKIT_PRIVATE_KEY = value
-			case "IMAGEKIT_URL_ENDPOINT":
-				c.IMAGEKIT_URL_ENDPOINT = value
-			case "UPSTASH_REDIS_REST_URL":
-				c.UPSTASH_REDIS_REST_URL = value
-			case "UPSTASH_REDIS_REST_TOKEN":
-				c.UPSTASH_REDIS_REST_TOKEN = value
-			}
-		}
-		c.ENVIRONMENT = environment
-	}
+        // Load environment variables dynamically
+        for _, key := range envVars {
+            value := os.Getenv(key)
+            if value == "" {
+                log.Warnf("%s is not set", key)
+            }
 
-	return c, nil
+            switch key {
+            case "ACCESS_TOKEN_SECRET":
+                c.ACCESS_TOKEN_SECRET = value
+            case "REFRESH_TOKEN_SECRET":
+                c.REFRESH_TOKEN_SECRET = value
+            case "PORT":
+                c.PORT = value
+            case "NEO4J_USER":
+                c.NEO4J_USER = value
+            case "NEO4J_PASSWORD":
+                c.NEO4J_PASSWORD = value
+            case "NEO4J_URI":
+                c.NEO4J_URI = value
+            case "TWILLIO_ACCOUNT_SID":
+                c.TWILLIO_ACCOUNT_SID = value
+            case "TWILLIO_ACCOUNT_PASSWORD":
+                c.TWILLIO_ACCOUNT_PASSWORD = value
+            case "TWILLIO_ACCOUNT_SERVICE_SID":
+                c.TWILLIO_ACCOUNT_SERVICE_SID = value
+            case "EMAIL_HOST":
+                c.EMAIL_HOST = value
+            case "EMAIL_PORT":
+                c.EMAIL_PORT = value
+            case "EMAIL_USER":
+                c.EMAIL_USER = value
+            case "EMAIL_PASSWORD":
+                c.EMAIL_PASSWORD = value
+            case "IMAGEKIT_PUBLIC_KEY":
+                c.IMAGEKIT_PUBLIC_KEY = value
+            case "IMAGEKIT_PRIVATE_KEY":
+                c.IMAGEKIT_PRIVATE_KEY = value
+            case "IMAGEKIT_URL_ENDPOINT":
+                c.IMAGEKIT_URL_ENDPOINT = value
+            case "UPSTASH_REDIS_REST_URL":
+                c.UPSTASH_REDIS_REST_URL = value
+            case "UPSTASH_REDIS_REST_TOKEN":
+                c.UPSTASH_REDIS_REST_TOKEN = value
+            }
+        }
+        c.ENVIRONMENT = environment
+    }
+
+    return c, nil
 }
